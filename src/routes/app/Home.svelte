81ef531ad0465f693f0bf779e418b12fcf19f109
SammarJuneja <sammarkumar345@gmail.com>
2024-06-28 23:41:08 +0530

    minor change

--- src/routes/app/Home.svelte
+++ src/routes/app/Home.svelte
@@ -6,22 +6,23 @@
 <div class="min-h-screen bg-app-bg">
     <Header />
     <div>
-        <div class="flex gap-2 border-b border-login-button p-2 py-3">
+    <!-- routing needed here -->
+        <div on:click={() => {alert('hi')}} class="flex w-full gap-2 border-b border-login-button p-2 py-3">
             <div class="flex items-center">
                 <img src={OpenChat} class="rounded-full" width="45px" alt="Open Chat">
             </div>
             <div class="grid">
                 <h2 class="text-white text-lg">Test1</h2>
-                <p class='text-gray-400'>Hello</p>
+                <p class="text-gray-400">Hello</p>
             </div>
         </div>
-        <div class="flex gap-2 border-b border-login-button p-2 py-3">
+        <div on:click={() => {alert('hi')}} class="flex w-full gap-2 border-b border-login-button p-2 py-3">
             <div class="flex items-center">
                 <img src={OpenChat} class="rounded-full" width="45px" alt="Open Chat">
             </div>
             <div class="grid">
                 <h2 class="text-white text-lg">Test2</h2>
-                <p class='text-gray-400'>Hi, How are you</p>
+                <p class="text-gray-400">Hi, How are you</p>
             </div>
         </div>
     </div>

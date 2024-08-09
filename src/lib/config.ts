d8b12295772e799a5f80e64f8a0d3d20d2101e13
SammarJuneja <sammarkumar345@gmail.com>
2024-08-09 16:26:46 +0530

    Fix login endpoint

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,9 +2,13 @@ require("dotenv");
 
 export const config: any = {
   mongo_uri: process.env.MONGO_URI,
-  jwtoken: {
-    secret: process.env.JWT_SECRET,
-    expiresIn: '1h',
+  jwtAccess: {
+    secret: process.env.ACCESS_SECRET,
+    expiresIn: "1h",
+  },
+  jwtRefresh: {
+    secret: process.env.REFRESH_TOKEN,
+    expiresin: "30d",
   },
   hashing: {
     saltRounds: 10,
--- src/routes/api/auth/login.ts
+++ src/routes/api/auth/login.ts
@@ -1,26 +1,45 @@
-import type { RequestHandler } from '@sveltejs/kit';
-import { signAccessToken, signRefreshToken } from '$lib/jwt';
-import mongoose from 'mongoose';
-import User from '$lib/modals/user';
+import { type RequestHandler, json } from "@sveltejs/kit";
+import { signAccessToken, signRefreshToken } from "$lib/jwt";
+import User from "$lib/modals/user";
+import bcrypt from "bcrypt";
 
 export const POST: RequestHandler = async ({ request }) => {
     const { email, password } = await request.json();
 
     if (!email && !password) {
-        return {
+        return json({
             "status": 400,
-            "error": "You didn\'t filled email or password"
-        }
+            "error": "You didn\'t fill email and password"
+        });
     }
 
-    const user = User.findOne({
+    const user: any = User.findOne({
         "email": email
     });
 
     if (!user) {
-        return {
+        return json({
             "status": 404,
             "error": "Username with that email was not found"
-        }
+        });
     }
+
+    const decodedPass = await bcrypt.compare(password, user.password);
+
+    if (!decodedPass) {
+        return json({
+            "status": 500,
+            "error": "You entered wrong password"
+        });
+    }
+
+    const accessToken = signAccessToken(user);
+    const refreshToken = signRefreshToken(user);
+    
+    return json({
+        "status": 200,
+        "message": "You are logged in successfully",
+        "accessToken": accessToken,
+        "refreshToken": refreshToken
+    });
 }

37ce8f78b191021e7cb7cff83f017e9aa8bb79a7
SammarJuneja <sammarkumar345@gmail.com>
2024-08-23 16:15:25 +0530

    Initilize functions

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
   import "../../app.css"
+
+  let email: string = "";
+  let password: string = "";
+
+  async function login() {
+
+  }
 </script>
 
 <div class="bg-app-bg min-h-screen grid items-center justify-center">
   <div>
     <div class="bg-log-tab grid items-center justify-center p-10 rounded-lg w-60">
-      <input class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all" placeholder="Email" type="text" required />
-      <input class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all mt-5" placeholder="Password" type="password" required />
+      <input bind:value={email} class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all" placeholder="Email" type="text" required />
+      <input bind:value={password} class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all mt-5" placeholder="Password" type="password" required />
     </div>
     <div class="grid mt-10 justify-center">
       <button class="p-2 w-40 bg-login-button text-white rounded-lg hover:bg-signup-button">
         Login
       </button>
-      <button class="text-sm text-white hover:text-blue-200">
+      <button on:click={login} class="text-sm text-white hover:text-blue-200">
         Forgot password?
       </button>
     </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-    import "../../app.css"
+    import "../../app.css";
+
+    let username = "";
+    let email: string = "";
+    let password: string = "";
+
+    async function signup() {
+
+    }
 </script>
 
 <div class="bg-app-bg min-h-screen grid items-center justify-center">
     <div>
     <div class="grid items-center justify-center p-5 bg-log-tab rounded-md">
-        <input type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Username">
-        <input type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Email">
-        <input type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Password">
+        <input bind:value={username} type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Username">
+        <input bind:value={email} type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Email">
+        <input bind:value={password} type="text" class="rounded-md my-2 p-2 outline-none bg-login-button hover:border hover:border-slate-200" placeholder="Password">
     </div>
     <div class="mt-10 grid items-center justify-center">
-        <button class="p-2 text-center text-white rounded-md bg-login-button hover:bg-signup-button">
+        <button on:click={signup} class="p-2 text-center text-white rounded-md bg-login-button hover:bg-signup-button">
             Signup
         </button>
-        <button class="mt-2 text-white text-sm hover:text-blue-200">
-            Already have an account? Register
-        </button>
+        <a href="/login" class="mt-2 text-white text-sm hover:text-blue-200">
+            Already have an account? Login
+        </a>
     </div>
 </div>
 </div>
